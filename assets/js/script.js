b4f16ade409ba2e5e374e1c9bd9b3e0a0ee82f41
mahesh <maheshmb7712@gmail.com>
2022-04-24 17:15:22 +0530

    hosting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,44 @@
-// Contact Me
-$("#gform").submit((e)=>{
-            e.preventDefault()
-            $.ajax({
-                url:"https://script.google.com/macros/s/AKfycbxnfbmns9KRBS1P8Jy2PGxFhTHqNIMDfL1TKujmkET4ZLtymT57Ihsss-xuKgE5b8M2/exec",
-                data:$("#gform").serialize(),
-                method:"post",
-                success:function (response){
-                    alert("Form submitted successfully")
-                    window.location.reload()
-                },
-                error:function (err){
-                    alert("Something Error")
-    
-                }
-            })
-        })
+    (document).ready(function(){
+// typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["FullStack Developer ", " Web Developer", "Freelancer",],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["FullStack Developer ", " Web Developer", "Freelancer",],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+
+    })
+
+
+
+
+
+
+
+// // Contact Me
+// $("#gform").submit((e)=>{
+//     e.preventDefault()
+//     $.ajax({
+//         url:"https://script.google.com/macros/s/AKfycbxnfbmns9KRBS1P8Jy2PGxFhTHqNIMDfL1TKujmkET4ZLtymT57Ihsss-xuKgE5b8M2/exec",
+//         data:$("#gform").serialize(),
+//         method:"post",
+//         success:function (response){
+//             alert("Form submitted successfully")
+//             window.location.reload()
+//         },
+//         error:function (err){
+//             alert("Something Error")
+
+//         }
+//     })
+// })
+
