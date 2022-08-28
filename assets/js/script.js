760a6e7a2567dad2059dd948f474864ad97f903b
Mahesh MB <90632100+MAHESH-M-B@users.noreply.github.com>
2022-08-28 16:08:37 +0530

    remove unused url in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@
 // $("#gform").submit((e)=>{
 //     e.preventDefault()
 //     $.ajax({
-//         url:"https://script.google.com/macros/s/AKfycbxnfbmns9KRBS1P8Jy2PGxFhTHqNIMDfL1TKujmkET4ZLtymT57Ihsss-xuKgE5b8M2/exec",
+//         url:"",
 //         data:$("#gform").serialize(),
 //         method:"post",
 //         success:function (response){
