1a3fa2d10654f1a0f487d0ba3c05924967011973
maheh <maheshmb7712@gmail.com>
2023-12-26 16:49:54 +0530

    updatin on 26-12

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 
 // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["FullStack Developer ", " Web Developer", "Freelancer",],
+        strings: [" Software Engineer","Web Developer", "Freelancer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["FullStack Developer ", " Web Developer", "Freelancer",],
+        strings: [" Software Engineer","Web Developer", "Freelancer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
